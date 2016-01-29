f76a99f9e32d2a0f89091647ac3812bf77c6f63e
Jiuck <jiuck.grecords@gmail.com>
2016-01-29 18:14:30 +0100

    Add urls and simple views

--- josegomez/web/urls.py
+++ josegomez/web/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    #url(r'^$', views.index, name='index'),
-    #url(r'^$', views.index, name='index'),
-    #url(r'^$', views.index, name='index'),
-    #url(r'^$', views.index, name='index'),
+    url(r'^$',          views.index,    name='index'),
+    url(r'^hello/$',    views.hello,  name='hello'),
+    url(r'^blog/$',     views.blog,     name='blog'),
+    url(r'^post/$',     views.post,     name='post'),
+    url(r'^projects/$', views.projects, name='projects'),
+    url(r'^aboutme/$',  views.aboutme,  name='aboutme'),
 ]
--- josegomez/web/views.py
+++ josegomez/web/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Buenas! Bienvenido a mi web personal!")
+
+def hello(request):
+	return HttpResponse("Buenas! Bienvenido al apartado hello!") 
+
+def blog(request):
+	return HttpResponse("Buenas! Bienvenido al apartado blog !") 
+
+def post(request):
+	return HttpResponse("Buenas! Bienvenido al apartado post !") 
+
+def projects(request):
+	return HttpResponse("Buenas! Bienvenido al apartado projects !") 
+
+def aboutme(request):
+	return HttpResponse("Buenas! Bienvenido al apartado aboutme !") 
