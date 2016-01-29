702a859c4ae57d2573351b5235b433b89630beab
Jiuck <jiuck.grecords@gmail.com>
2016-01-29 19:13:26 +0100

    Add post in models

--- josegomez/web/models.py
+++ josegomez/web/models.py
@@ -1,3 +1,22 @@
-from django.db import models
-
 # Create your models here.
+from django.db    import models 
+from django.utils import timezone
+ 
+class Post(models.Model): 
+    headline = models.CharField(max_length=255)
+    text     = models.TextField()
+    pub_date = models.DateTimeField()
+    mod_date = models.DateTimeField(default=timezone.now)
+    #author   = models.ManyToManyField(Author)
+    kudos    = models.IntegerField()
+    draft    = models.BooleanField(default=True)
+    image    = models.ImageField(upload_to = 'image/')
+    tags     = models.TextField()
+
+    def __str__(self):
+        return self.headline
+
+    def publish(self):
+        self.pub_date = timezone.now()
+        self.draft = False
+        self.save()
